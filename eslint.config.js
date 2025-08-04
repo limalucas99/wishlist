d0c06556a12948cd692d4e27ddd43407f08c2e8a
limalucas99 <lucaslim.lux@gmail.com>
2025-08-04 16:04:36 -0300

    chore: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+module.exports = (async function config() {
+  const { default: love } = await import('eslint-config-love')
+  
+  return [
+    {
+      ...love,
+      rules: {
+        ...love.rules,
+        '@typescript-eslint/class-methods-use-this': 'off',
+        '@typescript-eslint/strict-boolean-expressions': 'off', 
+        '@typescript-eslint/method-signature-style': 'off', 
+        'promise/avoid-new': 'off',
+      },
+      files: ['**/*.js', '**/*.ts'],
+      ignores: ["dist/**", "node_modules/**", "coverage/**"],
+    },
+  ]
+})()
