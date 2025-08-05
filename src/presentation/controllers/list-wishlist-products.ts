a9cd721380761961fbc4230b0821f0c30db6831a
limalucas99 <lucaslim.lux@gmail.com>
2025-08-05 02:06:39 -0300

    feat: ensure ListWishlistProductsController returns ok with wishlist data if wishlist exists

--- src/presentation/controllers/list-wishlist-products.ts
+++ src/presentation/controllers/list-wishlist-products.ts
@@ -3,7 +3,6 @@ import type { ListWishlistProductsDto } from "../dtos/list-wishlist-products.dto
 import { MissingParamError } from "../errors";
 import { badRequest, ok, serverError } from "../helpers/http-helper";
 import type { ListWishlistProducts } from "@/domain/usecases/list-wishlist-products";
-
 export class ListWishlistProductsController implements Controller {
   constructor(private readonly listWishlistProducts: ListWishlistProducts) {}
   async handle(request: ListWishlistProductsDto): Promise<HttpResponse> {
@@ -14,9 +13,11 @@ export class ListWishlistProductsController implements Controller {
 
       const { clientId } = request;
       const wishlist = await this.listWishlistProducts.list(clientId);
+      const EMPTY_PRODUCT_COUNT = 0;
 
       return ok({
         products: wishlist?.products ?? [],
+        totalProducts: wishlist?.products.length ?? EMPTY_PRODUCT_COUNT,
       });
     } catch (error) {
       return serverError();
