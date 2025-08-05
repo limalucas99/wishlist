2da7e562abc6453d8e9fe53b0f0207e6c8abcc86
limalucas99 <lucaslim.lux@gmail.com>
2025-08-05 20:52:53 -0300

    feat: implements remove method on WishlistRepository

--- src/infra/db/mongodb/wishlist-repository.ts
+++ src/infra/db/mongodb/wishlist-repository.ts
@@ -1,8 +1,11 @@
 import type { ProductModel } from "@/domain/models/product";
 import type { AddProductToWishlist } from "@/domain/usecases/add-product-to-wishlist";
 import { MongoHelper } from "./mongo-helper";
+import type { RemoveProductFromWishlist } from "@/domain/usecases/remove-product-from-wishlist";
 
-export class WishlistRepository implements AddProductToWishlist {
+export class WishlistRepository
+  implements AddProductToWishlist, RemoveProductFromWishlist
+{
   async add(product: ProductModel, clientId: string): Promise<void> {
     const wishlistCollection = MongoHelper.getCollection("wishlists");
     await wishlistCollection.updateOne(
@@ -13,5 +16,12 @@ export class WishlistRepository implements AddProductToWishlist {
       },
       { upsert: true }
     );
+  }
+  async remove(product: ProductModel, clientId: string): Promise<void> {
+    const wishlistCollection = MongoHelper.getCollection("wishlists");
+    const pullQuery: Record<string, unknown> = {
+      $pull: { products: product.id },
+    };
+    await wishlistCollection.updateOne({ clientId }, pullQuery);
   }
 }
