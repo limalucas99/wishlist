3f1d82b9a4720cf4eef86a020ad4a1e0773a60b7
limalucas99 <lucaslim.lux@gmail.com>
2025-08-04 21:41:11 -0300

    refactor: move http responses to a helper

---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,23 @@
+import { HttpStatusCode } from "../enums/http";
+import { ServerError } from "../errors";
+import type { HttpResponse } from "../protocols/http";
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: HttpStatusCode.BAD_REQUEST,
+  body: error,
+});
+
+export const serverError = (): HttpResponse => ({
+  statusCode: HttpStatusCode.INTERNAL_SERVER_ERROR,
+  body: new ServerError(),
+});
+
+export const ok = (data: unknown): HttpResponse => ({
+  statusCode: HttpStatusCode.OK,
+  body: data,
+});
+
+export const notFound = (error: Error): HttpResponse => ({
+  statusCode: HttpStatusCode.NOT_FOUND,
+  body: error,
+});
